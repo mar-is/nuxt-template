32bf1c3231d5ea98773a01b1cd4904ad6b668a2f
Marasi <marasi.mbp@gmail.com>
2020-04-16 17:41:21 +0700

    .

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,17 +2,17 @@ module.exports = {
   root: true,
   env: {
     browser: true,
-    node: true,
+    node: true
   },
   parserOptions: {
-    parser: 'babel-eslint',
+    parser: 'babel-eslint'
   },
   extends: [
     '@nuxtjs',
     'prettier',
     'prettier/vue',
     'plugin:prettier/recommended',
-    'plugin:nuxt/recommended',
+    'plugin:nuxt/recommended'
   ],
   plugins: ['prettier'],
   // add your custom rules here
@@ -20,7 +20,7 @@ module.exports = {
     'no-debugger': process.env.NODE_ENV == 'development' ? 'error' : 'off',
     'no-console':
       process.env.NODE_ENV == 'development'
-        ? ['error', { allow: ['warn', 'error'] }]
-        : 'off',
-  },
+        ? ['error', { allow: ['warn', 'error', 'log'] }]
+        : 'off'
+  }
 }
