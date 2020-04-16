2953eb8dec9aaf6e72e53a7e5d0dbdad14c68db4
Marasi <marasi.mbp@gmail.com>
2020-04-16 18:18:26 +0700

    .

--- .eslintrc.js
+++ .eslintrc.js
@@ -17,9 +17,10 @@ module.exports = {
   plugins: ['prettier'],
   // add your custom rules here
   rules: {
+    // Only allow debugger in development
     'no-debugger': process.env.PRE_COMMIT ? 'error' : 'off',
     'no-console': process.env.PRE_COMMIT
-      ? ['error', { allow: ['warn', 'error', 'log'] }]
+      ? ['error', { allow: ['warn', 'error'] }]
       : 'off'
   }
 }
--- pages/index.vue
+++ pages/index.vue
@@ -32,7 +32,7 @@ export default {
     Logo
   },
 
-  created() {
+  mounted() {
     console.log('console.log')
   }
 }
